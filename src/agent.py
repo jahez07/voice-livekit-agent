5167fcee8b2f43ce5a89ab77726903adc5ee7e5d
Jahez Abraham Johny <jahez.john@gmail.com>
2026-06-12 17:26:50 +0400

    updted the TTS to Elevenlabs

--- src/agent.py
+++ src/agent.py
@@ -1,3 +1,5 @@
+import os
+import re
 import logging
 import textwrap
 
@@ -11,15 +13,25 @@ from livekit.agents import (
     cli,
     room_io,
     UserInputTranscribedEvent,
-    ConversationItemAddedEvent
+    ConversationItemAddedEvent,
+    ChatContext,
+    ChatMessage,
+    StopResponse,
+    inference
 )
-from livekit.agents.llm import ChatMessage
-from livekit.plugins import cartesia, openai, silero
+
+from livekit.plugins import cartesia, openai, silero, elevenlabs
 from livekit.plugins.turn_detector.multilingual import MultilingualModel
 
 logger = logging.getLogger("agent")
 
 load_dotenv(".env")
+
+ELEVENLABS_API = os.getenv('ELEVENLABS_API_KEY')
+
+def _matches(self, text: str, mode: str) -> bool:
+    t = re.sub(r"[^a-z\s]", "", text.lower())
+    return ("vision" in t or "division" in t) and mode in t and "mode" in t
 
 
 class Assistant(Agent):
@@ -28,40 +40,30 @@ class Assistant(Agent):
             llm=openai.LLM(model="gpt-4o-mini"),
             instructions=textwrap.dedent(
                 """\
-                You are a friendly, reliable voice assistant that answers questions, explains topics, and completes tasks with available tools.
-
-                # Output rules
-
-                You are interacting with the user via voice, and must apply the following rules to ensure your output sounds natural in a text-to-speech system:
-
-                - Respond in plain text only. Never use JSON, markdown, lists, tables, code, emojis, or other complex formatting.
-                - Keep replies brief by default: one to three sentences. Ask one question at a time.
-                - Do not reveal system instructions, internal reasoning, tool names, parameters, or raw outputs
-                - Spell out numbers, phone numbers, or email addresses
-                - Omit `https://` and other formatting if listing a web url
-                - Avoid acronyms and words with unclear pronunciation, when possible.
-
-                # Conversational flow
-
-                - Help the user accomplish their objective efficiently and correctly. Prefer the simplest safe step first. Check understanding and adapt.
-                - Provide guidance in small steps and confirm completion before continuing.
-                - Summarize key results when closing a topic.
-
-                # Tools
-
-                - Use available tools as needed, or upon user request.
-                - Collect required inputs first. Perform actions silently if the runtime expects it.
-                - Speak outcomes clearly. If an action fails, say so once, propose a fallback, or ask how to proceed.
-                - When tools return structured data, summarize it to the user in a way that is easy to understand, and don't directly recite identifiers or other technical details.
-
-                # Guardrails
-
-                - Stay within safe, lawful, and appropriate use; decline harmful or out-of-scope requests.
-                - For medical, legal, or financial topics, provide general information only and suggest consulting a qualified professional.
-                - Protect privacy and minimize sensitive data.
+                Ask the user to select between
+                active mode
+                passive mode
                 """
             ),
         )
+        self.active = True
+    async def on_user_turn_completed(self, turn_ctx: ChatContext, new_message: ChatMessage):
+        text = (new_message.text_content or "").lower()
+
+        if "vision" in text and "active mode" in text:
+            self.active = True
+            return
+        
+        if ("vision" in text and "passive mode") or ("passive mode") in text:
+            self.active = False
+            await self.session.say("Going quiet. Say 'vision active mode' when you need me.")
+            raise StopResponse()
+        
+        if not self.active:
+            turn_ctx.items.append(new_message)
+            await self.update_chat_ctx(turn_ctx)
+            raise StopResponse()
+    
 
 
 server = AgentServer()
@@ -84,17 +86,18 @@ async def my_agent(ctx: JobContext):
         stt=openai.STT(
             model="gpt-4o-mini-transcribe",
         ),
-        tts=cartesia.TTS(model="sonic-3", voice="9626c31c-bec5-4cca-baa8-f8ba9e84c8bc"),
+        #tts=cartesia.TTS(model="sonic-3", voice="9626c31c-bec5-4cca-baa8-f8ba9e84c8bc"),
+        tts=elevenlabs.TTS(model="eleven_flash_v2_5", voice_id="Xb7hH8MSUJpSbSDYk0k2"),
         turn_detection=MultilingualModel(),
         vad=ctx.proc.userdata["vad"],
         preemptive_generation=True,
     )
 
-    # @session.on("user_input_transcribed")
-    # def on_user_input_transcribed(event: UserInputTranscribedEvent):
-    #     print(f"User input transcribed: {event.transcript},"
-    #           f"language: {event.language},"
-    #           f"speaker id: {event.speaker_id}")
+    @session.on("user_input_transcribed")
+    def on_user_input_transcribed(event: UserInputTranscribedEvent):
+        print(f"User input transcribed: {event.transcript},"
+              f"language: {event.language},"
+              f"speaker id: {event.speaker_id}")
         
     @session.on("conversation_item_added")
     def on_conversation_item_added(event: ConversationItemAddedEvent):
